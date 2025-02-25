a7d6dc0dbdb11cda4f9059a12cc4d2f32cec649b
rodrigoreis99 <rodrigo.reis.prado@gmail.com>
2025-02-25 17:06:40 -0300

    Primeira versão do projeto XpDoHeroi

---
+++ ataqueDoHeroi/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ataqueDoHeroi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var heroi1 = new Heroi("Arthas", 30, "guerreiro");
+            var heroi2 = new Heroi("Merlin", 150, "mago");
+            var heroi3 = new Heroi("Shen", 25, "ninja");
+            var heroi4 = new Heroi("Bruce", 40, "monge");
+
+            heroi1.ExibirInformacoes();
+            heroi1.Atacar();
+
+            heroi2.ExibirInformacoes();
+            heroi2.Atacar();
+
+            heroi3.ExibirInformacoes();
+            heroi3.Atacar();
+
+            heroi4.ExibirInformacoes();
+            heroi4.Atacar();
+        }
+    }
+}
---
+++ ataqueDoHeroi/heroi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ataqueDoHeroi
+{
+    public class Heroi
+    {
+        public string Nome { get; }
+        public int Idade { get; }
+        public string Tipo { get; }
+
+        public Heroi(string nome, int idade, string tipo)
+        {
+            Nome = nome;
+            Idade = idade;
+            Tipo = char.ToUpper(tipo[0]) + tipo.Substring(1).ToLower();
+        }
+        public void Atacar()
+        {
+            string ataque = Tipo.ToLower() switch
+            {
+                "mago" => "usou magia",
+                "guerreiro" => "usou espada",
+                "monge" => "usou artes marciais",
+                "ninja" => "usou shuriken",
+                _ => "usou um ataque desconhecido"
+            };
+            Console.WriteLine($"O {Tipo} atacou usando {ataque}");
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}, Tipo: {Tipo}");
+        }
+    }
+}
